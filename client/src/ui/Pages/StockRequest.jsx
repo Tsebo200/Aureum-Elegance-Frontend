8c93d209f4ddd9bdde1273d876d1668c0d3f4bed
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2025-05-10 11:57:40 +0200

    Feat(Setup Page): Add Stock Request Page
    Add Stock Request Page and setup the development environment by creating Components and Pages folder and installing sass for styling

---
+++ client/src/ui/Pages/StockRequest.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './StockRequest.module.scss'
+
+function StockRequest() {
+  return (
+    <div >
+      <div className={styles.mainContainer}></div>
+    </div>
+  )
+}
+
+export default StockRequest
