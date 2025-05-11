35b1425def99139d7d6531274203209b8582b472
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2025-05-12 01:39:56 +0200

    Style(Structure): Add Placeholder Divs
    Add placeholders for Dashboards structure

--- client/src/ui/Pages/Dashboard/Dashboard.tsx
+++ client/src/ui/Pages/Dashboard/Dashboard.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-
-
+import styles from '../Dashboard/Dashboard.module.scss';
+import Sidebar from '../../Components/Sidebar/Sidebar';
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div>
+      <div className={styles.mainContainer}>
+        <div className={styles.left}>
+            <Sidebar />
+        </div>
+        <div className={styles.right}>
+           <h1 className={styles.dashboardHeading}>Dashboard</h1>
+             <div className={styles.horLine}></div>
+              <div className={styles.mainSection}>
+                <div className={styles.spacer}></div>
+                <h1 className={styles.alertsHeading}>Alerts</h1>
+                <div className={styles.cardSection}></div>
+                <h1 className={styles.graphsHeading}>Graphs</h1>
+                <div className={styles.graphsSection}></div>
+              </div>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
