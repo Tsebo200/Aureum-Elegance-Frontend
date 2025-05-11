632f82fbbeb966e08a4f34d00d1626e2680c57c2
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2025-05-12 00:33:26 +0200

    Refactor(Cleanup) Remove Unused Lines
    Remove lines of code that are not doing anything in App.tsx

--- client/src/ui/App.tsx
+++ client/src/ui/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import StockRequest from './Pages/StockRequest/StockRequest';
 import Dashboard from './Pages/Dashboard/Dashboard';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
