3daeb11c8d6fe49e71ce585067ea8219e2c9a629
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2025-05-11 23:54:10 +0200

    Refactor(File Extention): Change .jsx to .tsx
    Change file extension from .jsx to .tsx in order to eliminate the typescript import errors within the app.tsx file, a .tsx file is a typescript file that supports .jsx syntax. Create Dashboard file setup

--- client/src/ui/App.tsx
+++ client/src/ui/App.tsx
@@ -1,9 +1,8 @@
 import { useState } from 'react'
-// @ts-ignore
-// import reactLogo from './assets/react.svg'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import StockRequest from './Pages/StockRequest/StockRequest';
+import Dashboard from './Pages/Dashboard/Dashboard';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,25 +10,10 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<StockRequest/>}/>
+        <Route path="/" element={<Dashboard/>}/>
+        <Route path="/stock-request" element={<StockRequest/>}/>
+        {/* Add more routes as needed */}
         </Routes>
-      {/* <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </>
   )
 }
