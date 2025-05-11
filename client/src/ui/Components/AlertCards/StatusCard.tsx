7ad8b34c62847612b103ae50f65cbc944f3ef6a2
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2025-05-12 01:41:06 +0200

    Style(Cards): Add Components
    Add Card Components structure planning

---
+++ client/src/ui/Components/AlertCards/StatusCard.tsx
@@ -0,0 +1,13 @@
+import styles from '../StatusCards/StatusCard.module.scss'
+
+function StatusCard() {
+  return (
+    <div>
+        <div className={styles.Container}>
+
+        </div>
+    </div>
+  )
+}
+
+export default StatusCard
---
+++ client/src/ui/Components/AlertCards/StockCard/StockCard.tsx
@@ -0,0 +1,13 @@
+import styles from '../StatusCard.module.scss'
+
+function StockCard() {
+  return (
+    <div>
+        <div className={styles.cardContainer}>
+            
+        </div>
+    </div>
+  )
+}
+
+export default StockCard
