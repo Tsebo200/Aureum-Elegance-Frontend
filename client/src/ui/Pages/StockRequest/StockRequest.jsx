cc11b6da72e46d8c2165bc0986b510084bda7002
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2025-05-11 23:30:12 +0200

    Style(StockRequest): Change Widths
    Change StockRequest Page widths for input forms placeholders

--- client/src/ui/Pages/StockRequest/StockRequest.jsx
+++ client/src/ui/Pages/StockRequest/StockRequest.jsx
@@ -1,53 +1,81 @@
 import React from 'react'
 import styles from './StockRequest.module.scss'
+import Sidebar from '../../Components/Sidebar/Sidebar'
+// import logo from '../../assets/Wordmark Logo.png';
+
+// const Sidebar = () => {
+//   const navItems = [
+//     "Dashboard",
+//     "Production",
+//     "Stock Request",
+//     "Add Stock",
+//     "Warehouse Stock",
+//     "User Management",
+//     "Stock Management",
+//   ];
+//   return (
+//     <aside className={styles.sidebar}>
+//       <div className={styles.logo}>
+//         <img src={logo} alt="Company logo" />
+//       </div>
+//       <nav className={styles.navigation}>
+//         {navItems.map((item, i) => (
+//           <a key={i} href="#" className={styles['nav-item']}>
+//             {item}
+//           </a>
+//         ))}
+//       </nav>
+//     </aside>
+//   );
+// };
+
+
 
 function StockRequest() {
   return (
     <div >
       <div className={styles.mainContainer}>
         <div className={styles.left}>
-        <div className={styles.navbar}></div>
-                <div className={styles.right}>
-                  <h1 className={styles.stockRequestHeading}>Stock Request</h1>
-                  <div className={styles.horLine}></div>
-                  <div className={styles.bottomSection}>
-                    <div className={styles.spacer}></div>
-                    <div className={styles.formContainer}>
-
-                      <div className={styles.topContainer}>
-                        <div className={styles.firstFormContainer}>
-                          <h3 className={styles.itemHeading}>Item Request</h3>
-                          <div className={styles.itemRequestForm}></div>
-                        </div>
-                        <div className={styles.secondFormContainer}>
-                            <h3 className={styles.itemHeading}>Warehouse To</h3>
-                            <div className={styles.WarehouseToForm}></div>
-                        </div>
-                        <div className={styles.thirdFormContainer}>
-                            <h3 className={styles.itemHeading}>Warehouse From</h3>
-                            <div className={styles.WarehouseFromForm}></div>
-                        </div>
-                      </div>
-
-                      <div className={styles.middleContainer}>
-                        <div className={styles.fourthFormContainer}>
-                        <h3 className={styles.itemHeading}>Amount in kilograms or litres</h3>
-                          <div className={styles.amountForm}></div>
-                        </div>
-                      </div>
-                      <div className={styles.bottomContainer}>
-                        <div className={styles.fifthFormContainer}>
-                          <h4 className={styles.submitHeading}>Submit Stock Transfer</h4>
-                          <div className={styles.amountForm}></div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-               
+          {/* <div className={styles.navbar}></div> */}
+            <Sidebar />
         </div>
+        <div className={styles.right}>
+          <h1 className={styles.stockRequestHeading}>Stock Request</h1>
+          <div className={styles.horLine}></div>
+          <div className={styles.bottomSection}>
+            <div className={styles.spacer}></div>
+            <div className={styles.formContainer}>
 
+              <div className={styles.topContainer}>
+                <div className={styles.firstFormContainer}>
+                  <h3 className={styles.itemHeading}>Item Request</h3>
+                  <div className={styles.itemRequestForm}></div>
+                </div>
+                <div className={styles.secondFormContainer}>
+                    <h3 className={styles.itemHeading}>Warehouse To</h3>
+                    <div className={styles.WarehouseToForm}></div>
+                </div>
+                <div className={styles.thirdFormContainer}>
+                    <h3 className={styles.itemHeading}>Warehouse From</h3>
+                    <div className={styles.WarehouseFromForm}></div>
+                </div>
+              </div>
 
+              <div className={styles.middleContainer}>
+                <div className={styles.fourthFormContainer}>
+                <h3 className={styles.itemHeading}>Amount in kilograms or litres</h3>
+                  <div className={styles.amountForm}></div>
+                </div>
+              </div>
+              <div className={styles.bottomContainer}>
+                <div className={styles.fifthFormContainer}>
+                  <h4 className={styles.submitHeading}>Submit Stock Transfer</h4>
+                  <div className={styles.amountForm}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
