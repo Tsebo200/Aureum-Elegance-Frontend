27e23b1b6a82604033e45caf4a8dcfcf6556e33a
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2025-05-11 19:18:06 +0200

    Style(Layout): Create Layout Stock Request Page
    Create layout for stock request. Need add material ui for form placeholders and Roboto for typeface

--- client/src/ui/Pages/StockRequest/StockRequest.jsx
+++ client/src/ui/Pages/StockRequest/StockRequest.jsx
@@ -9,7 +9,42 @@ function StockRequest() {
         <div className={styles.navbar}></div>
                 <div className={styles.right}>
                   <h1 className={styles.stockRequestHeading}>Stock Request</h1>
+                  <div className={styles.horLine}></div>
+                  <div className={styles.bottomSection}>
+                    <div className={styles.spacer}></div>
+                    <div className={styles.formContainer}>
+
+                      <div className={styles.topContainer}>
+                        <div className={styles.firstFormContainer}>
+                          <h3 className={styles.itemHeading}>Item Request</h3>
+                          <div className={styles.itemRequestForm}></div>
+                        </div>
+                        <div className={styles.secondFormContainer}>
+                            <h3 className={styles.itemHeading}>Warehouse To</h3>
+                            <div className={styles.WarehouseToForm}></div>
+                        </div>
+                        <div className={styles.thirdFormContainer}>
+                            <h3 className={styles.itemHeading}>Warehouse From</h3>
+                            <div className={styles.WarehouseFromForm}></div>
+                        </div>
+                      </div>
+
+                      <div className={styles.middleContainer}>
+                        <div className={styles.fourthFormContainer}>
+                        <h3 className={styles.itemHeading}>Amount in kilograms or litres</h3>
+                          <div className={styles.amountForm}></div>
+                        </div>
+                      </div>
+                      <div className={styles.bottomContainer}>
+                        <div className={styles.fifthFormContainer}>
+                          <h4 className={styles.submitHeading}>Submit Stock Transfer</h4>
+                          <div className={styles.amountForm}></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
+               
         </div>
 
 
