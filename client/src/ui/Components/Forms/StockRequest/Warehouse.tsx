e8097b086d1264216e74f034a3263866b56ea013
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2025-05-13 12:47:13 +0200

    Style(MUI Select Form): Add component
    Material UI Select Form add as a component, further imported within the StockRequest.tsx file

---
+++ client/src/ui/Components/Forms/StockRequest/Warehouse.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { FormControl, FormHelperText, InputLabel, MenuItem, Select} from '@mui/material';
+import type { SelectChangeEvent } from '@mui/material';
+
+function WarehouseTo() {
+  const [age, setAge] = React.useState<string>(''); // ensure string type
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <FormControl sx={{ m: 0.1, minWidth: 250 }}>
+      <InputLabel id="demo-simple-select-helper-label">Warehouse Selection</InputLabel>
+      <Select
+       sx={{borderRadius: 10, background: '#FFDBB1'}}
+        labelId="demo-simple-select-helper-label"
+        id="demo-simple-select-helper"
+        value={age}
+        label="Warehouse Selection"
+        onChange={handleChange}
+      >
+        <MenuItem value="">
+          <em>None</em>
+        </MenuItem>
+        <MenuItem value="10">Ten</MenuItem>
+        <MenuItem value="20">Twenty</MenuItem>
+        <MenuItem value="30">Thirty</MenuItem>
+      </Select>
+      {/* <FormHelperText>With label + helper text</FormHelperText> */}
+    </FormControl>
+  );
+}
+
+export default WarehouseTo;
