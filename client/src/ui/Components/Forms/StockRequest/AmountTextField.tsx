ad452869e8600adb9ffbc1ceb9d64bfe5cea7afc
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2025-05-14 10:58:14 +0200

    Feat(AmountTextField): Add Validation
    Add Validation within the AmountTextField component to only allow numbers to be entered

--- client/src/ui/Components/Forms/StockRequest/AmountTextField.tsx
+++ client/src/ui/Components/Forms/StockRequest/AmountTextField.tsx
@@ -13,6 +13,7 @@ export default function BasicTextFields() {
       id="outlined-basic" 
       label="Enter Amount" 
       variant="outlined" 
+      type="number"
       sx={{ '& .MuiOutlinedInput-root':{borderRadius: 10, background: '#FFDBB1'}}}/>
     </Box> // Adding '& .MuiOutlinedInput-root': before sx helps specify where(class) to add the styling 
     // Apply these styles to the element inside the current component that has the class MuiOutlinedInput-root
