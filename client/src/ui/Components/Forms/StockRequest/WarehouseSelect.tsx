141d7dc6de3a9272884c4a67fd3769a6a2318190
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2025-05-14 11:01:42 +0200

    Feat(WarehouseSelect): Change Select Option
    Change Select Option to Centurion and Cape Town

--- client/src/ui/Components/Forms/StockRequest/WarehouseSelect.tsx
+++ client/src/ui/Components/Forms/StockRequest/WarehouseSelect.tsx
@@ -23,9 +23,8 @@ function WarehouseTo() {
         <MenuItem value="">
           <em>None</em>
         </MenuItem>
-        <MenuItem value="10">Ten</MenuItem>
-        <MenuItem value="20">Twenty</MenuItem>
-        <MenuItem value="30">Thirty</MenuItem>
+        <MenuItem value="10">Centurion</MenuItem>
+        <MenuItem value="20">CapeTown</MenuItem>
       </Select>
       {/* <FormHelperText>With label + helper text</FormHelperText> */}
     </FormControl>
