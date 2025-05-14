233810a868bb349bf0865899212872a40c879634
TshwetsoMo <221411@virtualwindow.co.za>
2025-05-14 09:54:00 +0200

    Making the sidebar a component
    instead of using my original code withthe sidebar nav inlaid, I will make use of the sidebar component instead

--- client/src/ui/Pages/AddStock/AddStock.tsx
+++ client/src/ui/Pages/AddStock/AddStock.tsx
@@ -1,35 +1,10 @@
 import React from "react";
 import "./AddStock.scss";
-import logo from '../../assets/Wordmark Logo.png';
+import Sidebar from "../../Components/Sidebar";
 
 export default function AddStock() {
-  // Sidebar
-  const Sidebar = () => {
-    const navItems = [
-      "Dashboard",
-      "Production",
-      "Stock Request",
-      "Add Stock",
-      "Warehouse Stock",
-      "User Management",
-      "Stock Management",
-    ];
-    return (
-      <aside className="sidebar">
-        <div className="logo">
-          <img src={logo} alt="Company logo" />
-        </div>
-        <nav className="navigation">
-          {navItems.map((item, i) => (
-            <a key={i} href="#" className="nav-item">
-              {item}
-            </a>
-          ))}
-        </nav>
-      </aside>
-    );
-  };
 
+  
   // WarehouseSelector
   const WarehouseSelector = () => (
     <div className="selector-container">
