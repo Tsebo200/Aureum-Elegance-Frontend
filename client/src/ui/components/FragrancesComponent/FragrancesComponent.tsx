d6d3ade2deee65f5ab77818dc5b935770ea9d2b4
Sean-D-231121 <231121@virtualwindow.co.za>
2025-05-26 16:44:55 +0200

    add api services on fragranceComponents

--- client/src/ui/components/FragrancesComponent/FragrancesComponent.tsx
+++ client/src/ui/components/FragrancesComponent/FragrancesComponent.tsx
@@ -1,21 +1,24 @@
 import { Button } from "@mui/material";
 import styles from "./FragrancesComponent.module.scss";
-
+import { getFragrances } from "../../services/FragranceServiceRoute"
+import type { Fragrance } from "../../services/models/fragranceModel";
+import { useEffect, useState } from "react";
 const FragrancesComponent = () => {
-    const fragrances = [
-      {
-        name: "Moonlit Jasmine",
-        ingredients: ["Bergamot Oil", "Vanillin Powder", "Stabilizers"],
-        costPerUnit: 45.0,
-        amountInStock: 100,
-      },
-      {
-        name: "Citrus Breeze",
-        ingredients: ["Lemon Oil", "Orange Extract", "Preservatives"],
-        costPerUnit: 38.5,
-        amountInStock: 80,
-      },
-    ];
+  const [fragrances, setFragrances] = useState<Fragrance[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getFragrances(); 
+        console.log("Fetched fragrances:", data);
+        setFragrances(data); 
+      } catch (error) {
+        console.error("Error fetching fragrances:", error);
+      }
+    };
+
+    fetchData();
+  },[])
 return (
   <section className={styles.content}>
     <h1>Fragrances</h1>
@@ -28,15 +31,18 @@ return (
           <th>Amount in Stock</th>
           <th>Actions</th>
         </tr>
-        <hr />
       </thead>
       <tbody>
-        {fragrances.map((fragrance, index) => (
-          <tr key={index}>
+        {fragrances.map((fragrance) => (
+          <tr key={fragrance.id}>
             <td>{fragrance.name}</td>
-            <td>{fragrance.ingredients.join(", ")}</td>
-            <td>R {fragrance.costPerUnit.toFixed(2)}</td>
-            <td>{fragrance.amountInStock}</td>
+            <td>
+              {fragrance.fragranceIngredients
+                ?.flatMap((fi) => fi.ingredients?.map((i) => i.name))
+                .join(", ")}
+            </td>
+            <td>R {fragrance.cost.toFixed(2)}</td>
+            <td>{fragrance.volume}</td>
             <td>
               <Button className={styles.Btn}>Produce</Button>
               <Button className={styles.Btn}>Delete</Button>
