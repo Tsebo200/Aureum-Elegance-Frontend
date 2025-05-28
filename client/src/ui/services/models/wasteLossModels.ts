9ed9e70c672a7ccf9479894179dc6b0a172e8550
Sean-D-231121 <231121@virtualwindow.co.za>
2025-05-28 13:49:51 +0200

    Add WastelossIngredients and WastelossFragrance routes

---
+++ client/src/ui/services/models/wasteLossModels.ts
@@ -0,0 +1,54 @@
+export interface GetWastelossFragrance {
+  id: number;
+  quantityLoss: number;
+  reason: string;
+  dateOfLoss: string;
+
+  fragrance?: {
+    id: number;
+    name: string;
+    description: string;
+    cost: number;
+    expiryDate: string;
+    volume: number;
+  };
+
+  user?: {
+    userId: number;
+    name: string;
+    role: string;
+  };
+
+  warehouse?: {
+    warehouseID: number;
+    name: string;
+    location: string;
+  };
+}
+export interface GetWastelossIngredient {
+  id: number;
+  quantityLoss: number;
+  reason: string;
+  dateOfLoss: string;
+
+  Ingredients?: {
+    IngredientsId: number;
+    name: string;
+    type: string;
+    cost: number;
+    expiryDate: string;
+    isExpired: boolean;
+  };
+
+  user?: {
+    userId: number;
+    name: string;
+    role: string;
+  };
+
+  warehouse?: {
+    warehouseID: number;
+    name: string;
+    location: string;
+  };
+}
