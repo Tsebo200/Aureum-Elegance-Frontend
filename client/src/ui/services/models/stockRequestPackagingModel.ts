910125954b37fa6d915071384c751df3c719ea38
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2025-05-28 20:11:18 +0200

    Feat(StockRequestIngredient): Setup Service
    Setup Service Backend Integration for StockRequestIngredients

---
+++ client/src/ui/services/models/stockRequestPackagingModel.ts
@@ -0,0 +1,9 @@
+export interface StockRequestPackaging{
+    id: number;
+    amountRequest: number;
+    status: string;
+    requestDate: string;
+    packagingId: number;
+    userId: number;
+    warehouseId: number;
+}
