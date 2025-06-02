02703128b1172543a7c5b6dba7d89f4c8050e073
TshwetsoMo <221411@virtualwindow.co.za>
2025-06-02 10:38:48 +0200

    Adding the packaging component to our existing code

--- client/src/ui/Pages/Packaging/Packaging.tsx
+++ client/src/ui/Pages/Packaging/Packaging.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import styles from "./PackagingPanel.module.scss";
+import styles from "./Packaging.module.scss";
 import type { Packaging } from "../../services/models/packagingModel";
 import { getAllPackaging } from "../../services/packagingServiceRoute";
 
