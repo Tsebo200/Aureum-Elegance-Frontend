6f028af72b75f8b041507cfe7d303cd64f9efd7f
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2025-06-08 23:25:57 +0200

    Test(jest.config.ts): Fix Setup
    Require the new config as the globals.ts-jest approach is deprecated

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -2,15 +2,15 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-    preset: 'ts-jest', //library managing our tests
-    testEnvironment: 'jsdom', //Test environment
-    globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.test.json',  // 👈 Explicitly use the test config
-    },
+    preset: 'ts-jest/presets/default',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { isolatedModules: true }],
   },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 
 
 }
 export default config;
+
+
